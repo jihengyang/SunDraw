d2299fe2ce4ddc286b416f98e14af0db0d852d63
hengyangji <hengyangji@tencent.com>
2022-10-14 21:08:21 +0800

    提交RV测试代码

--- app/src/main/java/com/sun/sundraw/MainActivity.kt
+++ app/src/main/java/com/sun/sundraw/MainActivity.kt
@@ -7,6 +7,8 @@ import android.os.Looper
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,26 +16,58 @@ class MainActivity : AppCompatActivity() {
 
     private val rootView by lazy { findViewById<ViewGroup>(R.id.root) }
 
-    private val sunTextStatic by lazy { findViewById<TextView>(R.id.text_static) }
+//    private val sunTextStatic by lazy { findViewById<TextView>(R.id.text_static) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //异步渲染
-        val root = findViewById<View>(R.id.root)
-
-        SunLog.log("start pre load")
-
-        SunViewPool.preLoad(this)
-
+//        SunViewPool.preLoad(this)
 //        mainHandler.postDelayed(Runnable {
 //            rootView.addView(SunViewPool.sunTextView!!)
 //        }, 2000)
 
-        mainHandler.postDelayed(Runnable {
-            sunTextStatic.text = "修改文字"
-        }, 2000)
+//        mainHandler.postDelayed(Runnable {
+//            SunSimpleTrace.startTrace()
+//            sunTextStatic.text = "修改文字"
+//            SunSimpleTrace.endTrace("set text")
+//        }, 2000)
+
+        showList()
+        findViewById<View>(R.id.perform_travel).setOnClickListener {
+            it.requestLayout()
+        }
     }
+
+    private fun showList() {
+        findViewById<RecyclerView>(R.id.recyclerview).run {
+            layoutManager = LinearLayoutManager(context)
+            adapter = object : RecyclerView.Adapter<SunViewHolder>() {
+                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SunViewHolder {
+                    return SunViewHolder(TextView(context).apply {
+                        text = "1"
+                        minHeight = 100
+                        textSize = 20f
+                        setOnClickListener {
+                            it.requestLayout()
+                        }
+                        layoutParams = RecyclerView.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT
+                        )
+                    })
+                }
+
+                override fun onBindViewHolder(holder: SunViewHolder, position: Int) {
+                    (holder.itemView as TextView).text = position.toString()
+                }
+
+                override fun getItemCount() = 100
+            }
+        }
+    }
+
+    class SunViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onDestroy() {
         super.onDestroy()
